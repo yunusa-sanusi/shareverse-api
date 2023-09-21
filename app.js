dfd4df6aa3f80d812672c06f4e3f15e51276a5e8
Hussain <hussain@pop-os.localdomain>
2023-09-21 14:32:19 +0100

    Added some security middleware to the app

--- app.js
+++ app.js
@@ -1,6 +1,11 @@
 require('dotenv').config();
 require('express-async-errors');
 
+const helmet = require('helmet');
+const cors = require('cors');
+const xss = require('xss-clean');
+const rateLimiter = require('express-rate-limit');
+
 const express = require('express');
 const fileUpload = require('express-fileupload');
 const cloudinary = require('cloudinary').v2;
@@ -22,6 +27,18 @@ app.use(
     useTempFiles: true,
   }),
 );
+
+app.set('trust proxy', 1);
+app.use(
+  rateLimiter({
+    windowMs: 15 * 60 * 1000, // 15 minutes
+    max: 100, // limit each IP to 100 requests per windowMs
+  }),
+);
+app.use(express.json());
+app.use(helmet());
+app.use(cors());
+app.use(xss());
 
 cloudinary.config({
   cloud_name: process.env.CLOUDINARY_NAME,
